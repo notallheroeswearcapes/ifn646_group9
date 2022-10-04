a11d16d758a841fb97e5f3082d0aa78a763faa20
matthias.eder <eder.matthias1@gmail.com>
2022-10-04 17:41:07 +1000

    added splitting into train, test, validation sets

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import pickle
 import time
+import random
 from os import listdir, makedirs
 from os.path import isfile, join, exists
 
 
 def load_data(data_path="raw_data", cache_file_name="combined_sleep_data", cache_dir_name="cached_data"):
     """
-    Loads all files from the given path. Performs preprocessing and returns the cleaned data as one large DataFrame.
+    Loads all files from the given path. Performs preprocessing and returns the cleaned data as three DataFrames:
+        training data, test data, and validation data.
 
     :param cache_dir_name: The name of the directory used to keep cached files.
     :param cache_file_name: The name of the pickle file (without ending) to contain the cached DataFrame.
     :param data_path: The path to the directory containing the raw input data.
-    :return: A DataFrame containing the concatenated sleep records.
+    :return: Three DataFrames containing the concatenated sleep records separated into train, test, and validation sets.
     """
-    cache_file = f"{cache_dir_name}/{cache_file_name}.pkl"
+    cache_file_train = f"{cache_dir_name}/{cache_file_name}_train.pkl"
+    cache_file_test = f"{cache_dir_name}/{cache_file_name}_test.pkl"
+    cache_file_val = f"{cache_dir_name}/{cache_file_name}_val.pkl"
 
     if not exists(cache_dir_name):
         makedirs(cache_dir_name)
@@ -30,18 +34,29 @@ def load_data(data_path="raw_data", cache_file_name="combined_sleep_data", cache
         data_concat = pd.concat(dataframes, ignore_index=True)
 
         # run preprocessing and return data
-        data = __preprocess_data(data_concat)
-        print(f"Combined input data has {data.shape[0]} entries after preprocessing.")
+        training_data, test_data, validation_data = __preprocess_data(data_concat)
+        print(f"Training data has {training_data.shape[0]} entries after preprocessing.")
+        print(f"Test data has {test_data.shape[0]} entries after preprocessing.")
+        print(f"Validation data has {validation_data.shape[0]} entries after preprocessing.")
 
         # dump data to a pickle file for caching
-        with open(cache_file, "wb") as file:
-            pickle.dump(data, file)
+        with open(cache_file_train, "wb") as file:
+            pickle.dump(training_data, file)
+        with open(cache_file_test, "wb") as file:
+            pickle.dump(test_data, file)
+        with open(cache_file_val, "wb") as file:
+            pickle.dump(validation_data, file)
     else:
         # load existing pickle file
-        with open(cache_file, "rb") as file:
-            data = pickle.load(file)
+        print("Loading cached files.")
+        with open(cache_file_train, "rb") as file:
+            training_data = pickle.load(file)
+        with open(cache_file_test, "rb") as file:
+            test_data = pickle.load(file)
+        with open(cache_file_val, "rb") as file:
+            validation_data = pickle.load(file)
 
-    return data
+    return training_data, test_data, validation_data
 
 
 def inform(df):
@@ -63,9 +78,10 @@ def __preprocess_data(data):
         1) Extract time and day from the timestamp and put them into new columns. Remove timestamp column.
         2) Perform outlier detection.
         3) Remove entries with NaN values.
+        4) Split into training, test, validation sets. Remove day column.
 
     :param data: The DataFrame containing combined input data.
-    :return: A DataFrame with the preprocessed data.
+    :return: Three DataFrames with the preprocessed data split into the relevant sets.
     """
 
     # day/time extraction
@@ -97,12 +113,16 @@ def __preprocess_data(data):
     data = data.dropna().reset_index(drop=True)
     print(f"{100-(len(data)/len_before)*100.0:.2f}% of the {len_before} entries were dropped.")
 
-    return data
+    # split data into training, test, validation sets
+    training, test, validation = __split_data(data)
+
+    return training, test, validation
 
 
 def __detect_outliers(df, iqr_factor=4):
     """
     Performs an outlier detection with regard to an interval of a multiple of the IQR.
+
     :param df: The DataFrame to check for outliers.
     :param iqr_factor: The factor for the IQR.
     :return: A boolean mask indicating outliers where True.
@@ -119,6 +139,7 @@ def __detect_outliers(df, iqr_factor=4):
 def __missing_data_analysis(df):
     """
     Checks a given DataFrame for missing values. Returns a DataFrame with the percentage of missing values per column.
+
     :param df: The original DataFrame to check.
     :return: A DataFrame containing the percentage of missing values per column and the mean percentage of missing
     values of the whole data.
@@ -128,6 +149,37 @@ def __missing_data_analysis(df):
     mv.sort_values(by="percent_missing", ascending=False, inplace=True)
     mv.reset_index(drop=True, inplace=True)
     return mv, mv['percent_missing'].mean()
+
+
+def __split_data(df, num_train=20, num_test=5):
+    """
+    Splits the combined DataFrame into training, test, validation sets. Takes a random set of days into account per
+    set that can be specified via parameters.
+
+    :param df: The original combined DataFrame.
+    :param num_train: The number of days to include in the training set.
+    :param num_test: The number of days to include in the test set.
+    :return: Three DataFrames for training, test, validation sets.
+    """
+    if num_train + num_test >= 27:
+        raise Exception("Ratio for train/test/validation split is incorrect!")
+    seq = range(1, 28)
+    random.seed(123456)
+
+    train_days = random.sample(seq, k=num_train)
+    seq = [x for x in seq if x not in train_days]
+    test_days = random.sample(seq, k=num_test)
+    val_days = [x for x in seq if x not in test_days]
+
+    train = df[df["day"].isin(train_days)]
+    test = df[df["day"].isin(test_days)]
+    val = df[df["day"].isin(val_days)]
+
+    train.drop("day", axis=1, inplace=True)
+    test.drop("day", axis=1, inplace=True)
+    val.drop("day", axis=1, inplace=True)
+
+    return train, test, val
 
 
 if __name__ == '__main__':
